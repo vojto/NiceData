a1aa95e81dac3d794bd567935c0b54524884707f
Vojtech Rinik <vojto@rinik.net>
2017-04-25 15:06:41 +0200

    Initial Commit

---
+++ NiceData/NiceData.h
@@ -0,0 +1,19 @@
+//
+//  NiceData.h
+//  NiceData
+//
+//  Created by Vojtech Rinik on 4/25/17.
+//  Copyright © 2017 Median. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NiceData.
+FOUNDATION_EXPORT double NiceDataVersionNumber;
+
+//! Project version string for NiceData.
+FOUNDATION_EXPORT const unsigned char NiceDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NiceData/PublicHeader.h>
+
+
